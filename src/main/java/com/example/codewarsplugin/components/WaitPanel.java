a2b60f61690af22195282562897fc1276402a842
ansis@home <ansis@home>
2023-07-13 14:01:31 +0300

    icon

--- src/main/java/com/example/codewarsplugin/components/WaitPanel.java
+++ src/main/java/com/example/codewarsplugin/components/WaitPanel.java
@@ -1,5 +1,7 @@
 package com.example.codewarsplugin.components;
 
+import com.intellij.ui.AnimatedIcon;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,12 @@ public class WaitPanel extends JPanel {
     public WaitPanel(){
         super();
         ImageIcon imgIcon = new ImageIcon(this.getClass().getResource("/icons/giphy.gif"));
-        JLabel label = new JLabel(imgIcon);
+
+
+
+
+
+        JLabel label = new JLabel(new AnimatedIcon.Big());
         add(label, BorderLayout.NORTH);
     }
 }
