9ce73db7c4a63ad048c7d16546130f296621e4d7
ansis@home <ansis@home>
2023-08-01 14:02:26 +0300

    stop

--- src/main/java/com/example/codewarsplugin/components/DescriptionPanel.java
+++ src/main/java/com/example/codewarsplugin/components/DescriptionPanel.java
@@ -6,6 +6,9 @@ import com.example.codewarsplugin.state.Store;
 import javax.swing.*;
 import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DescriptionPanel extends JPanel {
 
@@ -41,9 +44,42 @@ public class DescriptionPanel extends JPanel {
     }
 
     private String getHtmlContent() {
-        return "<html> " + directory.getRecord().getDescription().replaceAll("\n", "<br>") + "</html>";
+
+        String rawString = directory.getRecord().getDescription();
+        String processedString = processLanguages(rawString, directory.getRecord().getSelectedLanguage().toLowerCase());
+
+
+        return "<html> " + processedString.replaceAll("\\n+", "<br>") + "</html>";
 
     }
+
+    private String processLanguages(String rawString, String language) {
+
+        ArrayList<String> tokens = extractTokens(rawString);
+        for (String token : tokens) {
+            System.out.println("token: " + token);
+            if (!token.toLowerCase().contains("```" + language) || token.toLowerCase().contains("```" + language + "script")) {
+                rawString = rawString.replace(token, "");
+                System.out.println("replace token: " + token);
+            }
+        }
+        return rawString.replace("```" + language, "").replace("```", "");
+    }
+
+    private static ArrayList<String> extractTokens(String inputString) {
+        ArrayList<String> tokens = new ArrayList<>();
+        Pattern pattern = Pattern.compile("```([a-zA-Z]+)\\n(.*?)```", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(inputString);
+
+        while (matcher.find()) {
+            String token = matcher.group(0);
+            tokens.add(token);
+        }
+
+        return tokens;
+    }
+
+
 
 
 }
